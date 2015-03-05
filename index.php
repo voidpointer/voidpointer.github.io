4b19df2d3e5d30c0a0c30f6278cdb62d3118549b
Ryan Jones <voidpointer@hotmail.com>
2015-03-05 13:16:45 -0600

    Better HTML.

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-Hi.
-<br><br><br><br>
-<img src="i/ryan_jones1.jpg" width="128" border="0">
+<!doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div class="page">
+			<div class="bio-pic">
+				<img src="i/ryan_jones1.jpg" width="128" border="0">
+			</div>
+		</div>
+	</body>
+</html>
