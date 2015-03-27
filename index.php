a2369aeeccb02e3c96805d2df713d3f6f1fbc17b
Ryan Jones <voidpointer@hotmail.com>
2015-03-27 16:10:52 -0500

    Added a lot of actual resume content.

--- index.php
+++ index.php
@@ -1,17 +1,143 @@
-<?
-require "spoof.php";
-die;
-?>
 <!doctype html>
 <html>
 	<head>
+		<link rel="stylesheet" href="normalize.css">
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
 		<div class="page">
-			<div class="bio-pic">
-				<img src="i/ryan_jones1.jpg" width="128" border="0">
-			</div>
+			<nav>
+				<div class="bio-pic">
+					<img src="i/ryan_jones1.jpg" width="128" border="0">
+				</div>
+			</nav>
+			<article>
+				<section>
+					<header>
+						<h1>Overview</h1>
+					</header>
+				</section>
+				<section>
+					<header>
+						<h1>Computer Science / Technical Skills</h1>
+					</header>
+				</section>
+				<section>
+					<header>
+						<h1>Professional Experience</h1>
+					</header>
+					<div class="position-company">				
+						<span class="position-title">Senior Software Engineer</span>, FatWallet.com, Beloit, Wisconsin
+					</div>
+					<div class="date-range">
+						2009-2014
+					</div>
+					<div style="clear:both;">&nbsp;</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title"><a href="http://www.fatwallet.com/" target="_blank">FatWallet.com</a></span> &ndash; FatWallet is a deals website
+						</div>
+						<div class="project-description">
+							I worked on every aspect of the FatWallet website (front end, back end, user facing pages, admin tools, reports).
+							<ul>
+								<li>The majority of my time was spent using PHP, MySQL, HTML, CSS, and JS</li>
+								<li>Used the following JavaScript libraries frequently: jQuery, jQuery UI, jQuery Mobile, Ext.js, Facebook JavaScript SDK</li>
+								<li>Worked with various Google stuff: GA, GA event tracking, GTM, Google DFP</li>
+								<li>Was usually the go to guy for improving the performance of SQL queries</li>
+								<li>Facebook</li>
+								<li>OpenGraph</li>
+								<li>Agile, scrum</li>
+								<li>Other: svn, git, FBML, REST, CrazyEgg</li>
+							</ul>
+						</div>
+					</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title"><a href="http://www.fatwallet.com/forums/?viewMobile=1" target="_blank">FatWallet.com Mobile Site</a></span> &ndash; Simplified version of the site for mobile devices
+						</div>
+						<div class="project-description">
+							Created the mobile version of the FatWallet site using jQuery Mobile on the front end.  There was originally a lot more to the mobile site, but now its just the forums.
+						</div>
+					</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title"><a href="https://itunes.apple.com/us/app/black-friday/id295358197?mt=8" target="_blank">FatWallet Black Friday iOS App</a></span> &ndash; iOS app to access FatWallet's Black Friday Deal Finder
+						</div>
+						<div class="project-description">
+							Maintained the Black Friday iOS App &ndash; mostly tweaks, bug fixes, and a few small improvements.
+						</div>
+					</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title">C.A.T.S. (Complex Anything Tracking System)</span> &ndash; FatWallet internal tool to track metrics
+						</div>
+						<div class="project-description">
+							...
+						</div>
+					</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title"><a href="http://www.pushpinsapp.com/" target="_blank">PushPinsApp.com</a></span> &ndash; Website version of the Push Pins mobile app
+						</div>
+						<div class="project-description">
+							...
+						</div>
+					</div>
+					<hr>
+					<div class="position-company">				
+						<span class="position-title">Senior Software Engineer</span>, Arachnid, Inc., Loves Park, Illinois		
+					</div>
+					<div class="date-range">
+						1998-2009
+					</div>
+					<div style="clear:both;">&nbsp;</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title"><a href="http://www.bullshooter.com/products-2/super-shuffle" target="_blank">Super Shuffle</a></span> &ndash; 3D Shuffleboard
+						</div>
+						<div class="project-description">
+							Designed and implemented a 3D video game version of table shuffleboard. Used C++, Blender, Blender Game Engine, and Delta3D, running on Ubuntu Linux.
+						</div>
+					</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title"><a href="http://www.bullshooter.com/products-2/galaxy-3-live" target="_blank">Galaxy 3 Live</a></span> &ndash; Dartboard
+						</div>
+						<div class="project-description">
+							Created a dartboard application with modernized graphics and Lua-scripted interfaces. Used C++, boost, Lua, SWIG, OpenGL, libSDL, libcurses, running on Slackware Linux.
+						</div>
+					</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title"><a href="https://www.leagueleader.net/" target="_blank">LeagueLeader</a></span> &ndash; On-line league management system
+						</div>
+						<div class="project-description">
+							Developed software and setup FreeBSD/Linux servers for an on-line dart league management system. Used PHP, HTML, CSS, JavaScript, MySQL, and DeZign for Databases.
+						</div>
+					</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title"><a href="http://www.bullshooter.com/products-2/galaxy-ii-5" target="_blank">Galaxy 2, Galaxy 2.5</a>, <a href="http://www.bullshooter.com/products-2/legacy-products-2/black-widow" target="_blank">Black Widow</a></span> &ndash; Dartboards
+						</div>
+						<div class="project-description">
+							Developed and maintained code in C for various subsystems, including game logic, sound playback, network/modem communication, graphics routines, a virtual ROM/RAM file system, memory management, etc., as well as startup code and low-level routines written in Coldfire assembly.
+						</div>
+					</div>
+					<div class="project">
+						<div class="project-name">
+							<span class="project-title">Miscellaneous Projects</span>
+						</div>
+						<div class="project-description">
+							Developed numerous other programs/utilities, including: PHP/ASP sites for tracking customer tech-support calls, software revisions, etc. Visual Basic programs for language translation, modem communication, font rasterization, etc.						
+						</div>
+					</div>
+				</section>
+				<section>
+					<header>
+						<h1>Education</h1>
+					</header>
+				</section>
+			</article>
 		</div>
 	</body>
 </html>
